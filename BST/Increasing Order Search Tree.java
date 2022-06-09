3c6de3e30cc27556e14a1c79a77ce766f051fad6
Shivani Vemula <76956616+shivanidotcom@users.noreply.github.com>
2022-06-09 12:33:31 +0530

    Create Increasing Order Search Tree.java

---
+++ BST/Increasing Order Search Tree.java
@@ -0,0 +1,48 @@
+Given the root of a binary search tree, rearrange the tree in in-order so that the leftmost node in the tree is now the root of the tree, and every node has no left child and only one right child.
+
+ 
+
+Example 1:
+
+
+Input: root = [5,3,6,2,4,null,8,1,null,null,null,7,9]
+Output: [1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
+Example 2:
+
+
+Input: root = [5,1,7]
+Output: [1,null,5,null,7]
+ 
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    TreeNode ans = new TreeNode();
+    TreeNode dummy = ans;
+    public void inorder(TreeNode root){
+        if(root==null) return ;
+        inorder(root.left);
+        dummy.right = new TreeNode(root.val);
+        dummy = dummy.right;
+        inorder(root.right);
+        
+    }
+    public TreeNode increasingBST(TreeNode root) {
+        inorder(root);
+        return ans.right;
+    }
+}
